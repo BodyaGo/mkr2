ad76cff15bb3b7d959a21710e596b04f7d1088f4
BodyaGo <mukhabodya@gmail.com>
2024-05-31 18:01:08 +0300

    add new path

--- project_recipe/urls.py
+++ project_recipe/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path
-
+from recipe.views import main, category_detail
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', main, name='main'),
+    path('category/<int:category_id>/', category_detail, name='category_detail'),
 ]
