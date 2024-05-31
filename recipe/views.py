bca83e080c76afbfdf198fbbd4b96d7d78a8d512
BodyaGo <mukhabodya@gmail.com>
2024-05-31 18:03:18 +0300

    add views

--- recipe/views.py
+++ recipe/views.py
@@ -7,10 +7,12 @@ def main(request):
     random_recipes = random.sample(recipes, min(len(recipes), 10))
     return render(request, 'main.html', {'recipes': random_recipes})
 
+
 def category_detail(request, category_id):
     category = get_object_or_404(Category, id=category_id)
     recipes = Recipe.objects.filter(category=category)
     return render(request, 'category_detail.html', {'category': category, 'recipes': recipes})
+
 
 def recipe_detail(request, recipe_id):
     recipe = get_object_or_404(Recipe, pk=recipe_id)
